54fb7b0f59a09464908037ba6c1c64080a43947f
Sue Doenim <anemailforvariouspurposes@gmail.com>
2021-02-06 17:22:21 -0800

    Trying to make a TH page

---
+++ docs/js/main.js
@@ -0,0 +1,15 @@
+function rollDice() {
+    arr = []
+    for (var i = 0; i < 4; i++) {
+        arr.push(3 * Math.floor((Math.random() * 3) - 1));
+    }
+    return arr;
+}
+
+function stringifyDice(dice, total) {
+    return `(${dice[0]}) + (${dice[1]}) + (${dice[2]}) + (${dice[3]}) = **${total}**`
+}
+
+function getTotal(dice) {
+    return dice.reduce((a, b) => a+b);
+}

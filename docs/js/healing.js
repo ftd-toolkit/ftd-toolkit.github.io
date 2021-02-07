f000254a559a002768c2b4a87b059f4d98868a13
Sue Doenim <anemailforvariouspurposes@gmail.com>
2021-02-07 10:20:11 -0800

    Incredibly sloppy full healing simulator.

--- docs/js/healing.js
+++ docs/js/healing.js
@@ -82,7 +82,76 @@ function healFixedDays() {
 }
 
 function healFull() {
+  inputs = getAllInputs("hd");
+  if (Object.entries(inputs).length !== 7) {
+    alert("Not all fields are filled in, or there is a datatype error. Fix and try again. Sorry for using alerts, but I didn't have a lot of patience to find a better way to do data validation.");
+    return;
+  }
+  // now all inputs are ints and we have the correct fields hopefully
   days_needed = 0
+  trauma = inputs["hd-trauma"];
+  total_healed = 0;
+  tn = inputs["hd-injury"];
+  severity = inputs["hd-severity"];
+  total_med = 0;
+  while (trauma > 0) {
+    healed = ($("#hd-rest").is(":checked") ? 10 : 0);
+    check = inputs["hd-stat"] + dice();
+    check -= tn;
+    if (check >= 0) {
+      healed += Math.floor(check / severity) + 1;
+    } else {
+      healed -= Math.floor(-check / severity) + 1;
+    }
+    if (inputs["hd-medknow"] > 0 && inputs["hd-mednin"] > 0) {
+      // Do full healing with mednin    
+      medknow = inputs["hd-medknow"] + dice();
+      mednin = inputs["hd-mednin"] + dice();
+      if (medknow < tn) {
+        mednin -= (tn - medknow);
+      }
+      mednin -= tn;
+      // Check if we're below 10 or below 20 thresholds to worsen condition...
+      if (mednin <= -20) {
+        healed -= 20;
+      } else if (mednin <= -10) {
+        healed -= 10;
+      }
+      medhealed = Math.floor(mednin * 2/severity);
+      total_med += medhealed;
+      trauma -= medhealed;
+    } else if (inputs["hd-mednin"] > 0) {
+      // Do partial healing with medknow
+      medknow = inputs["hd-medknow"] + dice();
+      medknow -= tn;
+      // Check if we're below thresholds...
+      if (medknow <= -20) {
+        healed -= 20;
+      } else if (medknow <= -10) {
+        healed -= 20;
+      }
+      medhealed = Math.floor(medknow/severity);
+      total_med += medhealed;
+      trauma -= medhealed;
+    }
+    total_healed += healed;
+    trauma -= healed;
+    days_needed += 1;
+  }
+  if (total_med === 0) {
+    result = "Healed " + total_healed + " trauma.";
+  } else {
+    result = "Healed " + total_healed + " trauma, and was healed for " + total_med + ".";
+  }
+
+  CUR_DAY += 1;
+  days = "Days " + CUR_DAY + "-" + (CUR_DAY + days_needed - 1);
+  CUR_DAY += (days_needed - 1);
+
+  roll = "Currently unused..."
+  addResultsToLog(days, roll, result);
+  trauma = Math.max(trauma, 0);
+  $("#hd-trauma").val(trauma);
 }
 
 
